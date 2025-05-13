726a09117931c156aff659ef24bee96a47419a91
ting-dev-coder <missjoker59@gmail.com>
2025-05-13 14:22:29 -0700

    Clean code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div className="flex min-h-screen justify-center bg-red-500">
-			<p>Hello world</p>
+			<p>Hello world!</p>
 		</div>
 	);
 }
