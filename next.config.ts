cad464ad28b0e008519ebfb6750c72f2c963360a
ting-dev-coder <missjoker59@gmail.com>
2025-05-21 14:35:52 -0700

    test drone.yml - 21

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	output: "export",
 	// basePath: "/jira-clone",
 	// assetPrefix: "/jira-clone/",
 	// output: "standalone",
