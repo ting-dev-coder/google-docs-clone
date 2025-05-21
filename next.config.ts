39cb37d72b7468700e7b2dfb4b737cf4e1db373c
ting-dev-coder <missjoker59@gmail.com>
2025-05-21 15:19:34 -0700

    test drone.yml - 24

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: "export",
-	// basePath: "/jira-clone",
-	// assetPrefix: "/jira-clone/",
-	// output: "standalone",
+	output: "export", // 靜態輸出
+	distDir: "jira-clone", // 改變輸出資料夾名稱
+	basePath: "/jira-clone", // 這裡是用來改網站基底路徑（替代 homepage）
+	assetPrefix: "/jira-clone/", // 靜態資源路徑前綴
 };
 
 export default nextConfig;
