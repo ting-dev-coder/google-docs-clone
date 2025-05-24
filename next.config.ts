b62685574bdd1388563dec1edf6a48e71abdb515
ting-dev-coder <missjoker59@gmail.com>
2025-05-23 18:58:45 -0700

    test ssr - 5

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	basePath: "/jira-clone",
-	assetPrefix: "/jira-clone/",
-	trailingSlash: true,
-	// 確保圖片能正常載入
 	images: {
 		unoptimized: false, // SSR 模式可以使用優化
 	},
