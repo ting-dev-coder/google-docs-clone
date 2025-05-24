e3a990dfc36ac5ac3767bfe68be6b6cdad9210a9
ting-dev-coder <missjoker59@gmail.com>
2025-05-23 18:05:19 -0700

    test ssr - 1

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: "export", // 靜態輸出
-	distDir: "jira-clone", // 改變輸出資料夾名稱
 	basePath: "/jira-clone", // 這裡是用來改網站基底路徑（替代 homepage）
 	assetPrefix: "/jira-clone/", // 靜態資源路徑前綴
 };
