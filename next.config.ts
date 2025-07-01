ecf222d3aeb6e056f5ac8786cd7b5aa63b1da073
tp27933 <missjoker59@gmail.com>
2025-07-02 00:41:48 +0800

    update cicd related files

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
 	basePath: isProd ? "/jira-clone" : "",
+	output: "standalone",
 	images: {
 		unoptimized: false, // SSR 模式可以使用優化
 	},
